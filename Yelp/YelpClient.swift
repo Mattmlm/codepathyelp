6760b5e03729fd0a5791228f0fd3d51ed0a4018d
Matt Mo <matthew.l.mo@gmail.com>
2015-09-25 15:29:26 -0700

    [Pod files] - fixing pod files (note this does NOT fix issues with pod install)

--- Yelp/YelpClient.swift
+++ Yelp/YelpClient.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AFNetworking
+import BDBOAuth1Manager
 
 // You can register for Yelp API keys here: http://www.yelp.com/developers/manage_api_keys
 let yelpConsumerKey = "vxKwwcR_NMQ7WaEiQBK_CA"
